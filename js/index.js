4cfd53d623582178e692b88ce6410ed4d791fe82
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-02-05 14:40:43 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -18,8 +18,7 @@ gsap.to("#subImg2", {
         trigger: "#subImg2", // 요소가 뷰포트에 드러나는 순간부터 애니메이션이 작동
         start: "top top", // 애니메이션 시작시점
         end:"bottom bottom", // 애니메이션 종료시점
-        scrub: 2,
-        marker:true
+        scrub: 2
     },
-    y: -200,duration:2,opacity:1
+    y: -250,duration:2,opacity:1
 });
