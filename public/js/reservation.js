db7fbebd94ce9677eac88fa64a712bc1074756a7
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-02-12 16:31:05 +0900

    no message

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -1,16 +1,21 @@
-const swiper = new Swiper('.img_slider', {
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
     loop: true,
 
-    // pagination: {
-    //     el: '.swiper-pagination',
-    // }
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
