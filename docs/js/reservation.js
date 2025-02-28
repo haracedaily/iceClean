df9393cc82a714e67e15da72c5453bf19c5b1d2a
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-02-28 11:47:09 +0900

    no message

--- docs/js/reservation.js
+++ docs/js/reservation.js
@@ -1,14 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log(Swiper);
     <!-- Initialize Swiper -->
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+    if(!!Swiper) {
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 4,
+            spaceBetween: 30,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    };
 });
 
 document.addEventListener("DOMContentLoaded", function () {
